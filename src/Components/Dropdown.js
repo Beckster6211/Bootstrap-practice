bc3dba77cba9f8db2848d0df4c0bf814f7ec5787
Beckster6211 <beckyljones151192@gmail.com>
2023-06-14 11:58:28 +0100

    started on dropdown

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,9 +1,224 @@
 import React from "react"
 
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton  from "react-bootstrap/DropdownButton";
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Button from 'react-bootstrap/Button';
+import SplitButton from 'react-bootstrap/SplitButton';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
 function ComDropdown(){
     return(
     <div>
         <p>Dropdown component</p>
+        <p>Single button dropdowns, wrapping Dropdown and inner DropdownMenu and DropdownToggle. By default the DropdownToggle will render Button and accepts all props</p>
+        <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Dropdown Button
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    <p>Provide title prop and some DropdownItems</p>
+    <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+    </DropdownButton>
+    <br/> 
+    {['Primary', 'Secondary', 'Success', 'Info', 'Warning', 'Danger'].map(
+        (variant) => (
+          <DropdownButton
+            as={ButtonGroup}
+            key={variant}
+            id={`dropdown-variants-${variant}`}
+            variant={variant.toLowerCase()}
+            title={variant}
+          >
+            <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+            <Dropdown.Item eventKey="3" active>
+              Active Item
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+          </DropdownButton>
+        ),
+      )}
+      <p>split button, can split dropdown from button</p>
+      <Dropdown as={ButtonGroup}>
+      <Button variant="success">Split Button</Button>
+
+      <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    <p>as with DropdownButton, SplitButton is provided</p>
+    {['Primary', 'Secondary', 'Success', 'Info', 'Warning', 'Danger'].map(
+        (variant) => (
+          <SplitButton
+            key={variant}
+            id={`dropdown-split-variants-${variant}`}
+            variant={variant.toLowerCase()}
+            title={variant}
+          >
+            <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+            <Dropdown.Item eventKey="3" active>
+              Active Item
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+          </SplitButton>
+        ),
+      )}
+        <p>Dropdown work with all size buttons</p>
+        <div className="mb-2">
+        {[DropdownButton, SplitButton].map((DropdownType, idx) => (
+          <DropdownType
+            as={ButtonGroup}
+            key={idx}
+            id={`dropdown-button-drop-${idx}`}
+            size="lg"
+            title="Drop large"
+          >
+            <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+            <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+          </DropdownType>
+        ))}
+      </div>
+      <div>
+        {[DropdownButton, SplitButton].map((DropdownType, idx) => (
+          <DropdownType
+            as={ButtonGroup}
+            key={idx}
+            id={`dropdown-button-drop-${idx}`}
+            size="sm"
+            variant="secondary"
+            title="Drop small"
+          >
+            <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+            <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+          </DropdownType>
+        ))}
+      </div>
+      <p>Dark Dropdown, matches dark navbars adding variant = "dark" onto DropdownMenu, alternatively use menuVariant="dark" using DropdownButton</p>
+      <Dropdown>
+        <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+          Dropdown Button
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu variant="dark">
+          <Dropdown.Item href="#/action-1" active>
+            Action
+          </Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item href="#/action-4">Separated link</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <DropdownButton
+        id="dropdown-button-dark-example2"
+        variant="secondary"
+        menuVariant="dark"
+        title="Dropdown button"
+        className="mt-2"
+      >
+        <Dropdown.Item href="#/action-1" active>
+          Action
+        </Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Item href="#/action-4">Separated link</Dropdown.Item>
+      </DropdownButton>
+      <p>Using menuVariant="dark" in a NavDropdown</p>
+      <Navbar variant="dark" bg="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-dark-example" />
+        <Navbar.Collapse id="navbar-dark-example">
+          <Nav>
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title="Dropdown"
+              menuVariant="dark"
+            >
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <p>drop directions, trigger dropdown menus above, below, left, or right of their toggle elements with drop prop</p>
+    <div className="mb-2">
+        {['up', 'up-centered', 'down', 'down-centered', 'start', 'end'].map(
+          (direction) => (
+            <DropdownButton
+              as={ButtonGroup}
+              key={direction}
+              id={`dropdown-button-drop-${direction}`}
+              drop={direction}
+              variant="secondary"
+              title={` Drop ${direction} `}
+            >
+              <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+              <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+              <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+            </DropdownButton>
+          ),
+        )}
+      </div>
+
+      <div>
+        {['up', 'up-centered', 'down', 'down-centered', 'start', 'end'].map(
+          (direction) => (
+            <SplitButton
+              key={direction}
+              id={`dropdown-button-drop-${direction}`}
+              drop={direction}
+              variant="secondary"
+              title={`Drop ${direction}`}
+            >
+              <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+              <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+              <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+            </SplitButton>
+          ),
+        )}
+      </div>
     </div>
     )
 }
