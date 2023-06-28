7ebc6abee6d55a6ff4780eeccca3e153695a0aec
Beckster6211 <beckyljones151192@gmail.com>
2023-06-28 11:16:47 +0100

    form range done

--- src/Components/Form/FormRange.js
+++ src/Components/Form/FormRange.js
@@ -1,9 +1,14 @@
 import React from "react"
 
+import Form from 'react-bootstrap/Form';
+
 function ComFormRange(){
     return(
         <div>
             <p>Form Range</p>
+            <p>Custom input type="range" controls with FormRange, track (background) and thumb (value) both styled the same</p>
+            <Form.Label>Range</Form.Label>
+            <Form.Range />
         </div>
     )
 }
